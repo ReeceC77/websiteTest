437468f16e394e155201bc81894c5a76e7274dc7
ReeceC77 <reece.clem7@gmail.com>
2024-12-08 21:50:13 -0500

    Update script-reading.js

--- script-reading.js
+++ script-reading.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Fetching real-time weather data as an example
   const fetchWeatherData = async () => {
     try {
-      const response = await fetch("https://api.weatherapi.com/v1/current.json?key=5ab9d4fb56af096ec97723caa6ec9c5a&q=London");
+      const response = await fetch("https://api.weatherapi.com/v1/current.json?key=5ab9d4fb56af096ec97723caa6ec9c5a=London");
       const data = await response.json();
       dataDiv.innerHTML = `Temperature in ${data.location.name}: ${data.current.temp_c}°C`;
     } catch (err) {
