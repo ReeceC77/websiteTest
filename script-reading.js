4eedd875e16dde5f9a905764bc51308816a05bb9
ReeceC77 <reece.clem7@gmail.com>
2024-12-08 21:51:35 -0500

    Update script-reading.js

--- script-reading.js
+++ script-reading.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dataDiv = document.getElementById("data");
 
-  // Fetching real-time weather data as an example
+  // Fetching real-time weather data
   const fetchWeatherData = async () => {
     try {
-      const response = await fetch("https://api.weatherapi.com/v1/current.json?key=5ab9d4fb56af096ec97723caa6ec9c5a=London");
+      const apiKey = "5ab9d4fb56af096ec97723caa6ec9c5a";
+      const city = "Cumberland";
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
+      
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
       const data = await response.json();
       dataDiv.innerHTML = `Temperature in ${data.location.name}: ${data.current.temp_c}°C`;
     } catch (err) {
